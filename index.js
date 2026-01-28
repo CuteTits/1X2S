65edb786393cc888b11854a4ea29444e9cbfa95f
CuteTits <xsperit@gmail.com>
2026-01-28 08:30:20 -0800

    Initial commit

--- index.js
+++ index.js
@@ -176,7 +176,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET || 'supersecretkey',
   resave: false,
   saveUninitialized: false,
-  cookie: { httpOnly: true, secure: false, maxAge: 1000 * 60 * 60 }
+  cookie: { httpOnly: true, secure: false }
 }));
 
 // --- Session status ---
